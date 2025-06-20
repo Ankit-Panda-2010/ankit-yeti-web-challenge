0473dffd8908fbb7e0b32ab06d5c0afe195a2242
Ankit-Panda-2010 <ankitpanda2010@gmail.com>
2025-06-20 10:48:39 -0400

    To do List Basic Features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
 "use client";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  function handleAddTodo() {
+    if (todo.trim() === "") return;
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+
+  function handleRemoveTodo(idx: number) {
+    setTodos(todos => todos.filter((_, i) => i !== idx));
+  }
+
   return (
     <div>
       <h1>YETI Web Challenge</h1>
@@ -9,6 +26,24 @@ export default function Home() {
         shadcn/ui.
       </p>
       {/* TODO: Your code here */}
+      <div className="flex gap-2 mt-4">
+        <Input placeholder="Input Todo..." value={todo}
+          onChange={e => setTodo(e.target.value)}
+          onKeyDown={e => { if (e.key === "Enter") handleAddTodo(); }}/>
+        <Button className="bg-blue-500 ..." onClick={handleAddTodo} >Add</Button>
+      </div>
+      <ul className="mt-6 space-y-2">
+        {todos.map((item, idx) => (
+          <li key={idx}>
+            <button
+              className="w-full text-left px-3 py-2 rounded border border-input bg-background hover:bg-accent transition cursor-pointer shadow-sm"
+              onClick={() => handleRemoveTodo(idx)}
+            >
+              {item}
+            </button>
+          </li>
+        ))}
+      </ul>
       {/* You can branch out of this file to create new files in the src/app/components folder if needed */}
     </div>
   );
